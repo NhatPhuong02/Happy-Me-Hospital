0deeaa76f051c4d29246cd9f002791fc691e678e
NaLiHg <49004582+linhlbn@users.noreply.github.com>
2023-02-27 09:54:51 +0700

    update password file ../config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
     // db info
     $db_host = '127.0.0.1';
     $db_user = 'root';
-    $db_password = '';
+    $db_password = 'root';
     $db_db = 'happyme';
     $db_port = 3306;
     $dbname = 'happyme';
