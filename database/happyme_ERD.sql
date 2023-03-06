f73eaa05b1248124187359527deab76fd28f5807
NaLiHg <49004582+linhlbn@users.noreply.github.com>
2023-03-06 14:41:15 +0700

    update DB follow ER diagram

--- database/happyme_ERD.sql
+++ database/happyme_ERD.sql
@@ -3,7 +3,7 @@
 -- Payment, Cart, OrderDetail, OrderMedicine
 
 CREATE TABLE IF NOT EXISTS `happyme`.`User` (
-  `u_id` INT NOT NULL,
+  `u_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `u_firstName` VARCHAR(255) NULL,
   `u_lastName` VARCHAR(255) NULL,
   `u_Phone` VARCHAR(10) NULL,
@@ -12,49 +12,45 @@ CREATE TABLE IF NOT EXISTS `happyme`.`User` (
   `u_gender` enum('Male','Female','Order') COLLATE utf8_vietnamese_ci NOT NULL,
   `u_role` enum('patient','therapist','admin') COLLATE utf8_vietnamese_ci NOT NULL,
   `u_address` VARCHAR(400) NULL,
-  `u_avatar` VARCHAR(45) NULL,
-  PRIMARY KEY (`u_id`));
+  `u_avatar` VARCHAR(45) NULL);
 
 
 CREATE TABLE IF NOT EXISTS `happyme`.`Medicine` (
-  `m_id` INT NOT NULL,
+  `m_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `m_name` VARCHAR(255) NULL,
   `m_quantity` VARCHAR(255) NULL,
   `m_price` INT NULL,
   `m_pic` VARCHAR(45) NULL,
-  `m_description` VARCHAR(400) NULL,
-  PRIMARY KEY (`m_id`));
+  `m_description` VARCHAR(400) NULL);
 
 CREATE TABLE IF NOT EXISTS `happyme`.`Payment` (
-  `p_id` INT NOT NULL,
+  `p_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `p_type` VARCHAR(45) NULL,
   `p_amount` VARCHAR(45) NULL,
-  `p_info` VARCHAR(45) NULL,
-  PRIMARY KEY (`p_id`));
+  `p_info` VARCHAR(45) NULL);
 
-CREATE TABLE IF NOT EXISTS `happyme`.`Cart` (
-  `c_id` INT NOT NULL,
-  `c_quantity` INT NOT NULL,
-  `User_u_id` INT NOT NULL,
-  `Medicine_m_id` INT NOT NULL,
-  PRIMARY KEY (`c_id`, `c_quantity`, `Medicine_m_id`, `User_u_id`),
-  CONSTRAINT `fk_Cart_User1`
-    FOREIGN KEY (`User_u_id`)
-    REFERENCES `happyme`.`User` (`u_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Cart_Medicine1`
-    FOREIGN KEY (`Medicine_m_id`)
-    REFERENCES `happyme`.`Medicine` (`m_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+CREATE TABLE IF NOT EXISTS happyme.Cart (
+c_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+c_quantity INT NOT NULL,
+User_u_id INT NOT NULL,
+Medicine_m_id INT NOT NULL,
+CONSTRAINT fk_Cart_User1
+FOREIGN KEY (User_u_id)
+REFERENCES happyme.User (u_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION,
+CONSTRAINT fk_Cart_Medicine1
+FOREIGN KEY (Medicine_m_id)
+REFERENCES happyme.Medicine (m_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+);
 
 CREATE TABLE IF NOT EXISTS `happyme`.`OrderDetail` (
-  `od_id` INT NOT NULL,
-  `o_total_price` VARCHAR(45) NULL,
+  `od_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `o_total_price` DECIMAL(10,2) NULL,
   `User_u_id` INT NOT NULL,
   `Payment_p_id` INT NOT NULL,
-  PRIMARY KEY (`od_id`, `User_u_id`, `Payment_p_id`),
   CONSTRAINT `fk_OrderDetail_User1`
     FOREIGN KEY (`User_u_id`)
     REFERENCES `happyme`.`User` (`u_id`)
@@ -68,12 +64,11 @@ CREATE TABLE IF NOT EXISTS `happyme`.`OrderDetail` (
 );
 
 CREATE TABLE IF NOT EXISTS `happyme`.`OrderMedicine` (
-  `om_id` INT NOT NULL,
+  `om_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `om_name` VARCHAR(45) NULL,
-  `om_quantity` VARCHAR(45) NULL,
+  `om_quantity` INT NULL,
   `OrderDetail_od_id` INT NOT NULL,
   `Medicine_m_id` INT NOT NULL,
-  PRIMARY KEY (`OrderDetail_od_id`, `Medicine_m_id`, `om_id`),
   CONSTRAINT `fk_OrderMedicine_OrderDetail1`
     FOREIGN KEY (`OrderDetail_od_id`)
     REFERENCES `happyme`.`OrderDetail` (`od_id`)
@@ -85,6 +80,7 @@ CREATE TABLE IF NOT EXISTS `happyme`.`OrderMedicine` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
+
 
 
 -- Thanh and Hung adds data here:
