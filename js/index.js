1229dcc6642043f98c5af15a361b12813ba175ba
Prin <phuongnhat443@gmail.com>
2023-03-07 22:58:22 +0700

    update js

--- js/index.js
+++ js/index.js
@@ -49,3 +49,19 @@ function create() {
     else
         alert("Welcome " + email.value + " back")
 }
+
+function add_amt(id) {
+    var amt_bx = document.getElementById(id);
+    var amt = amt_bx.value;
+    if (amt < 99) {
+        amt_bx.value++;
+    }
+}
+
+function sub_amt(id) {
+    var amt_bx = document.getElementById(id);
+    var amt = amt_bx.value;
+    if (amt > 1) {
+        amt_bx.value--;
+    }
+}
