c855ed19bc2a8e75309f4589445bb384db595c53
Minh Bùi Quang <minh982002@gmail.com>
2023-03-08 10:42:15 +0700

    cust_cart

--- cust_cart.php
+++ cust_cart.php
@@ -3,20 +3,20 @@
 
 <head>
     <?php
-        session_start();
-        include ('head.php');
-        include ('conn_db.php');
+    session_start();
+    include('head.php');
+    include('conn_db.php');
     ?>
     <title>Welcome Happy Me</title>
 </head>
 
 <body>
     <?php
-        include ('header.php');
+    include('header.php');
 
 
     ?>
-    
+
     <div class="container">
         <div class="row fs-3 bg-main mt-5 p-3 rounded-3">
             <div class="col-6">
@@ -33,7 +33,7 @@
             <div class="col-6 p-0">
                 <div class="row">
                     <div class="col-4 logo w-25">
-                        <img src= "./img/drugs/anafranil.jpeg" class="img-thumbnail bg-white" alt="logo">
+                        <img src="./img/drugs/anafranil.jpeg" class="img-thumbnail bg-white" alt="logo">
                     </div>
                     <div class="col-8">
                         <div class="title fs-4">Anafranil</div>
@@ -41,27 +41,26 @@
                     </div>
                 </div>
             </div>
-            <div class="col-2 align-middle text-center fs-4"><span>2</span></div>
-            <div class="col-2 align-middle text-center h-50 fs-4">100.000</div>
-           <div class="col-2 align-middle text-center ">
-            <div class="btn btn-outline-danger w-50 h-25 fs-4 p-0">Delete</div>
-           </div>
-        </div>
-        <div class="row justify-content-end">
-            <hr class=" my-3 bg-main w-50" size="7px">
-            <div class="w-100"></div>
-              <div class="col-2 p-2 h5">Subtotal</div>
-              <div class="col-2 p-2 h5">$36.00</div>
-              <div class="w-100"></div>
-              <div class="col-2 p-2 h5">Tax (10%)</div>
-              <div class="col-2 p-2 h5">$3.60</div>
-              <div class="w-100"></div>
-              <div class="col-2 p-2 h4">Total</div>
-              <div class="col-2 p-2 h4">$39.60</div>
-              <div class="w-100"></div>
-              <button type="button" class="btn btn-lg btn-light col-4 bg-main me-5 rounded-3" style="color: #FFF "> Proceed to checkout <i class="fa-sharp fa-solid fa-arrow-right"></i> </button>
+            <div class="col-2 my-5 text-center fs-4"><span>2</span></div>
+            <div class="col-2 my-5 text-center h-50 fs-4">100.000</div>
+            <div class="col-2 my-5 text-center ">
+                <button type="button" class="btn btn-lg btn-light col-4 bg-main rounded-3" style="color:#FFF "> <i class="fa-sharp fa-solid fa-trash-can"></i> </button>
             </div>
-          </div>
-          <?php include('footer.php')?>
+        </div>
+        <div class="row justify-content-end ">
+            <hr class=" my-3 w-50 bg-main" size="7px">
+            <div class="w-100"></div>
+            <div class="col-2 p-2 h5">Subtotal</div>
+            <div class="col-2 p-2 h5 text-end">360.000 VND</div>
+            <div class="w-100"></div>
+            <div class="col-2 p-2 h5">Tax (10%)</div>
+            <div class="col-2 p-2 h5 text-end">360.000 VND</div>
+            <div class="w-100"></div>
+            <div class="col-2 p-2 h4">Total</div>
+            <div class="col-2 p-2 h4 text-end">360.000 VND</div>
+            <div class="w-100"></div>
+            <button type="button" class="btn btn-lg btn-light col-4 bg-main me-1 rounded-3" style="color: #FFF "> Proceed to checkout <i class="fa-sharp fa-solid fa-arrow-right"></i> </button>
+        </div>
+        <?php include('footer.php') ?>
     </div>
 </body>
