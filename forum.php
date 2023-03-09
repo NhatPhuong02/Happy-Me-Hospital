868bbff235cda51a86389595255c7675782f19a2
Minh Bùi Quang <minh982002@gmail.com>
2023-03-09 20:53:38 +0700

    forum

--- forum.php
+++ forum.php
@@ -3,20 +3,32 @@
 
 <head>
     <?php
-        session_start();
-        include("head.php");
-        if (!isset($_SESSION['firstName'])) {
-            header("Location: signIn.php");
-            exit(1);
-        } 
+    session_start();
+    include("head.php");
+    if (!isset($_SESSION['firstName'])) {
+        header("Location: signIn.php");
+        exit(1);
+    }
     ?>
     <title>Our Feedback</title>
 </head>
 
 
 <body>
-<?php include("header.php")?>
-<?php include('footer.php')?>
+    <?php include("header.php") ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-4 bg-main rounded-start-5">
+            </div>
+            <div class="col-4 text-center ">
+                Top Categories
+            </div>
+            <div class="col-12">
+                Slide
+            </div>
+        </div>
+    </div>
+    <?php include('footer.php') ?>
 </body>
 
 </html>
