2fb10f4850a18dd42f1822ff8afde37c1a6cbfd8
Minh Bùi Quang <minh982002@gmail.com>
2023-03-13 22:11:51 +0700

    Booking

--- booking.php
+++ booking.php
@@ -29,24 +29,35 @@
     <div class="container">
         <div class="row mt-5">
             <div class="col-6">
-                <div class="h4">Booking an Appointment</div>
-                <div class="h5 mt-4" style="color: #138483;"> 12/03/2023 </div>
-                <div class="grid gap-3 ms-3 mt-5">
-                    <button type="button" class="btn btn-lg btn-primary p-2 g-col-6 me-3">Primary</button>
-                    <button type="button" class="btn btn-lg btn-primary p-2 g-col-6">Primary</button>
+                <p class="h4 fw-semibold">Booking an Appointment</p>
+                <p class="h5 mt-5 fw-semibold" style="color: #138483;"> 12/03/2023 </p>
+                <div class="grid gap-3 ms-3 mt-4">
+                    <button type="button" class="btn btn-outline-secondary p-2 g-col-6 me-3" style="width: 130px; color: black; ">9:30</button>
+                    <button type="button" class="btn btn-outline-secondary p-2 g-col-6" style="width: 130px; color: black;">9:30</button>
                     <br>
-                    <button type="button" class="btn btn-lg btn-primary p-2 g-col-6 mt-3 me-3">Primary</button>
-                    <button type="button" class="btn btn-lg btn-primary p-2 g-col-6 mt-3 ">Primary</button>
+                    <button type="button" class="btn btn-outline-secondary p-2 g-col-6 mt-3 me-3" style="width: 130px; color: black; ">9:30</button>
+                    <button type="button" class="btn btn-outline-secondary p-2 g-col-6 mt-3 " style="width: 130px; color: black;">9:30</button>
                 </div>
+                <div class="h5 mt-5 fw-semibold" style="color: #138483;"> 12/03/2023 </div>
+                <div class="grid gap-3 ms-3 mt-4">
+                <button type="button" class="btn btn-outline-secondary p-2 g-col-6 me-3" style="width: 130px; color: black;">9:30</button>
+                    <button type="button" class="btn btn-outline-secondary p-2 g-col-6" style="width: 130px; color: black;">9:30</button>
+                    <br>
+                    <button type="button" class="btn btn-outline-secondary p-2 g-col-6 mt-3 me-3" style="width: 130px; color: black;">9:30</button>
+                    <button type="button" class="btn btn-outline-secondary p-2 g-col-6 mt-3 " style="width: 130px; color: black;">9:30</button>
+                </div>
+                <br>
+                <div class="mt-5" style="margin-left: 91px"><button type="button" class="btn btn-outline-dark btn-lg"  >Book now</button></div>
+                
             </div>
             <div class="col-6 ">
-                <div class="card bg-main justify-content-center w-50">
+                <div class="card bg-main rounded-5 rounded-top-0 w-50 " style="margin-left: 23.5%;" >
                     <div class="card-body text-center ">
                         Dr. Bui Quang Minh
                     </div>
                 </div>
                 <div class="row justify-content-center">
-                    <div class=" col-12 card justify-content-center " style="width: 17rem; height:25rem;border: 10.2535px solid #FFF;
+                    <div class=" col-12 card justify-content-center mt-3" style="width: 17rem; height:25rem;border: 10.2535px solid #FFF;
                     border-radius: 117.915px 117.915px 5.12675px 5.12675px;background: linear-gradient(180deg, #BFFCFA 0%, #138483 100%); filter: drop-shadow(0px 5.12675px 41.014px rgba(0, 145, 193, 0.5)); ;">
                         <img class="card-img img-fluid col-12 justify-content-center text-center" src="img/image 8.png" alt="">
                     </div>
